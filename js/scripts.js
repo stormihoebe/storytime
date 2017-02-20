2aa47c6a760950c42b584ba823333d1c8280c083
Kaya Winn and Stormi Hoebelheinrich <stormi@>
2017-02-20 10:55:22 -0800

    clean up code and use loops to make the code less redundant

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 $(function() {
-  $("#first-form").submit(function(event) {
-    var person1Input = $("#person1").val();
-    var person2Input = $("#person2").val();
-    var animalInput = $("#animal").val();
-    var exclamationInput = $("#exclamation").val();
-    var verbInput = $("#verb").val();
-    var nounInput = $("#noun").val();
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
+  $("#first-form").submit(function(event) {
+
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb","noun"];
+    console.log(blanks)
+    event.preventDefault();
+    blanks.forEach(function(blank) {
+      var userInput = $("#" + blank).val();
+      $("." + blank).text(userInput);
+
+    });
 
     $("#first-form").hide(300);
     $("#story").show(500);
 
-    event.preventDefault();
+
   });
 });
